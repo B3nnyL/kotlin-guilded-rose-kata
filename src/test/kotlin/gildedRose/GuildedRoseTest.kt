626bb06b25c5b51ad18973eeb22ea1b11bb15719
B3nnyL <shuyinglin47@gmail.com>
2020-06-02 21:09:26 +1000

    [update] update readme and gradle test and gradle property

--- src/test/kotlin/gildedRose/GuildedRoseTest.kt
+++ src/test/kotlin/gildedRose/GuildedRoseTest.kt
@@ -1,21 +1,19 @@
 package gildedRose
 
-import gildedRose.GildedRose
-import gildedRose.Item
-import io.mockk.impl.annotations.MockK
-import com.natpryce.hamkrest.*
 import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 object GuildedRoseTest: Spek({
     describe("GildedRose") {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        it("should equal"){
-            assertThat("foo", equalTo(app.items[0].name))
+        context("update quality") {
+            val items = arrayOf<Item>(Item("foo", 0, 0))
+            val app = GildedRose(items)
+            app.updateQuality()
+            it("should equal") {
+                assertThat("foo", equalTo(app.items[0].name))
+            }
         }
-
     }
 })
